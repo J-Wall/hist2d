76acc16bf94c87cafa644f33108d5346770b979e
Jesse Wallace <jesse.wallace@anu.edu.au>
2019-07-03 15:44:41 +1000

    first version

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
-use std::env;
-use std::fs;
-use std::io::{BufRead, BufReader, Result};
+#[macro_use]
+extern crate clap;
+use clap::App;
 
-fn main() -> Result<()> {
-    let mut args = env::args();
-    args.next();
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
-    let filename = match args.next() {
-        Some(arg) => arg,
-        None => Err("No input file specified"),
-    }
+fn main() {
+    let yml = load_yaml!("cli.yml");
+    let m = App::from_yaml(yml).get_matches();
 
-    let file = File::open(filename)?;
+    let nbins = value_t!(m, "nbins", usize).unwrap();
+
+    let mut hist: Vec<Vec<u32>> = vec![vec![0; nbins]; nbins];
+
+    let file = File::open(m.value_of("INPUT").unwrap())
+        .expect("Error reading file");
     for line in BufReader::new(file).lines() {
+        let line = line.unwrap();
+        let mut vals = line.split_whitespace();
+        vals.next();
+        let c1: usize = vals.next().unwrap().parse().expect("Invalid file format");
+        let c2: usize = vals.next().unwrap().parse().expect("Invalid file format");
+        hist[c1][c2] += 1;
     }
 
-    Ok(())
+    for row in hist {
+        let line = row.into_iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(m.value_of("sep").unwrap());
+        println!("{}", line);
+    }
 }
